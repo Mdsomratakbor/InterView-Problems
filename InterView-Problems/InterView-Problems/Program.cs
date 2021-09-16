46a5a493e98394d5c27b46304ffc96a4234590ce
Mdsomratakbor <somrat876@gmail.com>
2021-09-16 23:11:47 +0600

    project initialize

---
+++ InterView-Problems/InterView-Problems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterView_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDimantionalArray = new int[2, 3];
+            Console.WriteLine("Please enter 2D Array Elements :");
+            for(int i=0; i<2; i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    twoDimantionalArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            int rows = twoDimantionalArray.GetLength(0);
+           int columns =   twoDimantionalArray.GetLength(1);
+            for(int column = 0; column < columns; column++)
+            {
+                for(int row =0; row < rows; row++)
+                {
+                    Console.Write($"{twoDimantionalArray[row, column]} ");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
