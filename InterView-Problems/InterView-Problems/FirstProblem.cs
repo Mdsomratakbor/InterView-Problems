136ec43acf18a225bde5edc92296885e782bc610
Mdsomratakbor <somrat876@gmail.com>
2021-09-16 23:15:31 +0600

    add class for first problem

---
+++ InterView-Problems/InterView-Problems/FirstProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterView_Problems
+{
+    public class FirstProblem
+    {
+        public static void PrintOneDArrayByColumn ()
+        {
+            int[,] twoDimantionalArray = new int[2, 3];
+            Console.WriteLine("Please enter 2D Array Elements :");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    twoDimantionalArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            int rows = twoDimantionalArray.GetLength(0);
+            int columns = twoDimantionalArray.GetLength(1);
+            for (int column = 0; column < columns; column++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    Console.Write($"{twoDimantionalArray[row, column]} ");
+                }
+            }
+        }
+    }
+}
--- InterView-Problems/InterView-Problems/Program.cs
+++ InterView-Problems/InterView-Problems/Program.cs
@@ -6,24 +6,7 @@ namespace InterView_Problems
     {
         static void Main(string[] args)
         {
-            int[,] twoDimantionalArray = new int[2, 3];
-            Console.WriteLine("Please enter 2D Array Elements :");
-            for(int i=0; i<2; i++)
-            {
-                for(int j=0; j<3; j++)
-                {
-                    twoDimantionalArray[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            int rows = twoDimantionalArray.GetLength(0);
-           int columns =   twoDimantionalArray.GetLength(1);
-            for(int column = 0; column < columns; column++)
-            {
-                for(int row =0; row < rows; row++)
-                {
-                    Console.Write($"{twoDimantionalArray[row, column]} ");
-                }
-            }
+           FirstProblem.PrintOneDArrayByColumn();
             Console.ReadKey();
 
         }
